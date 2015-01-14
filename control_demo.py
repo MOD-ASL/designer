f391c2e1cbc423bc4413304590963472a61a836a
jimjing <jimjing131313@gmail.com>
2015-01-13 21:05:26 -0500

    make control_demo.py warn when gait file does not exist

--- control_demo.py
+++ control_demo.py
@@ -1,12 +1,25 @@
 #! /usr/bin/env python
 from builders.control_builder import ControlBuilder
+import os, sys
+
+if len(sys.argv) < 2:
+    print "Usage: python control_demo.py path/to/control_file.control"
+    sys.exit(0)
 
 # initialize ControlBuilder
 CB = ControlBuilder()
 
+##############################################################
+####### Change these path to the right directory #############
 # define some basic gait files
-pick = "/home/asl/SMORES/gaits_and_configs/simple/pick.gait"
-roll = "/home/asl/SMORES/gaits_and_configs/simple/roll.gait"
+front = "/home/asl/SMORES/gaits_and_configs/vleg/front.gait"
+back = "/home/asl/SMORES/gaits_and_configs/vleg/back.gait"
+normal = "/home/asl/SMORES/gaits_and_configs/vleg/normal.gait"
+##############################################################
+
+if not os.path.isfile(front):
+    print "Please change the path of the gait files to the right directory."
+    sys.exit(0)
 
 # build basic gait block
 f1 = CB.createGaitBlockET("finger1", pick)
@@ -19,4 +32,4 @@ blockET = CB.createParallelBlockET([f1, f2])
 blockET = CB.createSeriesBlockET([blockET, f3, bodyroll])
 
 # save to control file
-CB.build(blockET, "/home/asl/SMORES/designer/data_files/grabbot/grabbot.control")
+CB.build(blockET, sys.argv[1])

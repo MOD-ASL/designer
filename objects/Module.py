7c11f7147da86eb2abee04ec2c53100516dbf511
jimjing <jimjing131313@gmail.com>
2015-01-13 21:11:25 -0500

    clean up connection and module object

--- objects/Module.py
+++ objects/Module.py
@@ -1,44 +1,20 @@
-## @package Module Module object that used by configuration editor
-
-## This is the class that stores all the information about a module
 class Module:
-  ## Constructor
-  # @param self Object pointer
-  # @param modelname Model name string
-  # @param position Tuple of the initial position of the module
-  # @param jointangle List of the joint angles of the module
-  # @param path Path of the model sdf file
-  # @quapos Whether the orientation of the module stored in quaternion 
-  #         True: stored in quaternion; False: stored in Eular angles; 
-  #         Default: False
-  def __init__(self, modelname,position,jointangle, path, quapos = False):
-    ## Module name strong
-    self.ModelName = modelname
-    try:
-      ## Module position tuple
-      self.Position = position
-    except ValueError:
-      pass
-    try:
-      ## Module joint angle list
-      self.JointAngle = jointangle
-    except ValueError:
-      pass
-    ## Module node dictionary
-    # Keys are the node indecies and values are pointer to the Connection object
-    self.nodes = {0:[],1:[],2:[],3:[]}
-    ## Path of the module sdf file
-    self.Path = path
-    ## Flag of whether the orientation of the module stored in quaternion format
-    self.Quaternion = quapos
-  ## Connect on one node
-  # @param self Object pointer
-  # @param node A node index
-  # @param connection A Connection object
-  def connection(self,node,connection):
-  	self.nodes[node] = connection
-  ## Disconnect on one node
-  # @param self Object pointer
-  # @param node A node index
-  def disconnect(self,node):
-  	self.nodes[node] = []
+    def __init__(self,modelname, position, jointangle, path, quapos = False):
+        self.ModelName = modelname
+        try:
+            self.Position = position
+        except ValueError:
+            pass
+        try:
+            self.JointAngle = jointangle
+        except ValueError:
+          pass
+        self.nodes = {0:False, 1:False, 2:False, 3:False}
+        self.Path = path
+        self.Quaternion = quapos
+
+    def connect(self, node):
+        self.nodes[node] = True
+
+    def disconnect(self, node):
+        self.nodes[node] = False

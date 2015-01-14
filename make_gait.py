c0c4045dbdf21f25b100570b171f7e3250bc5184
jimjing <jimjing131313@gmail.com>
2015-01-13 20:55:00 -0500

    enable self-collision checking when making gait file

--- make_gait.py
+++ make_gait.py
@@ -12,9 +12,9 @@ CB.saveNewConfiguration(file_name+".conf")
 GB = GaitBuilder()
 GB.loadControl(file_name+".control")
 
-#GCC = GaitCollisionChecker()
-#gait = GCC.loadGait(GB.output_str.split("\n"))
-#GCC.runGait(CB)
+GCC = GaitCollisionChecker()
+gait = GCC.loadGait(GB.output_str.split("\n"))
+GCC.runGait(CB)
 
 GB.output_str = CB.replaceAllModuleName(GB.output_str)
 GB.saveNewGait(file_name+".gait")
